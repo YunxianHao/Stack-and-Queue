d36b799ef5415c00af6af90be5896b6c41aa9505
YunxianHao <92800800+YunxianHao@users.noreply.github.com>
2022-05-17 12:21:59 +0800

    Create Stack.h

---
+++ Stack/Stack.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+typedef int STDataType;
+typedef struct Stack
+{
+	STDataType* a;
+	int top;
+	int capacity;
+}ST;
+//Init
+void StackInit(ST* ps);
+//destroy
+void StackDestroy(ST* ps);
+//enter the stack
+void StackPush(ST* ps, STDataType x);
+//out of stack
+void StackPop(ST* ps);
+//return stack top element
+STDataType StackTop(ST* ps);
+//Judge empty
+bool StackEmpty(ST* ps);
+//the counts
+int StackSize(ST* ps);
